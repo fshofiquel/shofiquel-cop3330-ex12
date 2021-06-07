d0d1e4b56afdb5b3c994bd2325a79a5a4d2fe4bf
fshofiquel <fshofique@gmail.com>
2021-06-06 22:08:55 -0400

    update

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -23,17 +23,17 @@ public class App
         DecimalFormat dF = new DecimalFormat();
         dF.setGroupingUsed(false);
 
-        System.out.print("Enter the principal: ");
+        System.out.print("Enter the principal? ");
         principle = prinIn.nextFloat();
-        System.out.print("Enter the rate of interest: ");
+        System.out.print("Enter the rate of interest? ");
         ratePer = rateIn.nextFloat();
-        System.out.print("Enter the number of years: ");
+        System.out.print("Enter the number of years? ");
         years = yearsIn.nextInt();
 
         rateDec = ratePer / 100;
         investment = principle * (1 + (rateDec * years));
 
-        output = "After" +years+ "years at " +ratePer+"%, the investment will be worth $"+dF.format(investment);
+        output = "After " + years + " years at " + ratePer + "%, the investment will be worth $" + dF.format(investment);
 
         System.out.print(output);
 
